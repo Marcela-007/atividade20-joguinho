1300421a3212ed2fbc94e355c9d0d91add0378b5
Marcela-007 <marcelaoliveira446677@gmail.com>
2023-10-23 14:26:31 -0300

    atividade20-adivinhe o número

---
+++ marcela.py
@@ -0,0 +1,14 @@
+# Exercício Python 28: Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 5
+# e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.
+# O programa deverá escrever na tela se o usuário venceu ou perdeu.
+# DICA ESTUDEM A BIBLIOTECA PYTHON RANDOM
+from random import randint
+numero = randint(0,5)
+print('')
+print('Vou sortear um número de 0 a 5.')
+print('')
+usuario = int(input('Adivinhe o número: '))
+if usuario == numero:
+    print('VOCÊ VENCEU🎉!!')
+else:
+    print('VOCÊ PERDEU😜')
